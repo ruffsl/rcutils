a0b590dea17674fa11fee28c1c7dd79fbed4067b
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-09-10 16:25:21 -0700

    try C string literals

---
+++ src/string_literals.c
@@ -0,0 +1,12 @@
+#include <uchar.h>
+void func()
+{
+    const char * l1 = "plain";
+    (void) l1;
+    const char * l2 = u8"utf-8";
+    (void) l2;
+    const char16_t * l3 = u"utf-16";
+    (void) l3;
+    const char32_t * l4 = U"uft-32";
+    (void) l4;
+}
